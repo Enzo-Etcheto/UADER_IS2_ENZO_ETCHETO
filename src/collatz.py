b6fea0938568b8d4540bf0acd43b50caa95cb0a2
Enzo-Etcheto <127917105+Enzo-Etcheto@users.noreply.github.com>
2024-03-23 22:11:43 -0300

    calculo_collatz

---
+++ src/collatz.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def collatz(n):
+
+    secuencia = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        secuencia.append(n)
+    return secuencia
+
+numeros = np.arange(1, 10001)
+longitudes = []
+for numero in numeros:
+    secuencia = collatz(numero)
+    longitud = len(secuencia)
+    longitudes.append(longitud)
+
+plt.plot(numeros, longitudes)
+plt.xlabel("Número inicial")
+plt.ylabel("Número de iteraciones")
+plt.title("Conjetura de Collatz (2n+1)")
+plt.show()

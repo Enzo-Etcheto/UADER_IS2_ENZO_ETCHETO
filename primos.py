0a970fca8cf011391b240d325882ee4c5d85e723
Enzo-Etcheto <127917105+Enzo-Etcheto@users.noreply.github.com>
2024-03-23 21:12:32 -0300

    comentarios_agredados

--- primos.py
+++ primos.py
@@ -1,13 +1,18 @@
 # Python program to display all the prime numbers within an interval
+#Intervalo de numeros 
+#limite inferior
 lower = 1
+#limite superior
 upper = 100
+#imprime la leyenda "los numeros primos comprendidos son:"
 print("Prime numbers between", lower, "and", upper, "are:")
 
 
 for num in range(lower, upper + 1):
+   # Todos los numeros primos mayores que 1 
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
-               break 
+               break #Sale del bucle si el numero no es primo
        else:
-           print(num) 
+           print(num) #Imprime el número si es primo

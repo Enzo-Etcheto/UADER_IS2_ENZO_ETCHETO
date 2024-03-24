9f80fff42b8572fe5c2303e9218316c22de7bf88
Enzo-Etcheto <127917105+Enzo-Etcheto@users.noreply.github.com>
2024-03-23 21:49:20 -0300

    calcula_rango

--- src/factorial.py
+++ src/factorial.py
@@ -13,8 +13,32 @@ def factorial(num):
             num -= 1
         return fact 
 
+
+def calcular_factoriales(rango):
+    
+    if "-" not in rango:
+        inicio = 1
+        fin = int(rango)
+    
+    elif rango.startswith("-"):
+        inicio = 1
+        fin = int(rango[1:])
+    
+    elif rango.endswith("-"):
+        inicio = int(rango[:-1])
+        fin = 60
+    
+    else:
+        inicio, fin = map(int, rango.split('-'))
+    
+    for num in range(inicio, fin + 1):
+        print("Factorial de", num, "! es", factorial(num))        
+
+
 if len(sys.argv) < 2:
-   num=int(input("Debe informar un numero :"))
+    print("Si no se coloca alguno de los limites los preterminados seran inicio=1, fin=60")
+    rango = input("ingrese el rango de numeros en el formato 'desde-hasta': ")
 else:
-    num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+    rango=sys.argv[1]
+
+calcular_factoriales(rango)
